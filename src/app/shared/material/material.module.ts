18331fda971e9d83cf1de791e2ee9f99ec8f83bb
Lyubomyr Kuzenko <l.kuzenko@levi9.com>
2018-10-22 16:16:56 +0300

    add @angular/material module

---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,127 @@
+import { NgModule } from '@angular/core';
+
+// @angular/materials modules
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule
+} from '@angular/material';
+
+// @angular/cdk modules
+// import { DragDropModule } from '@angular/cdk/drag-drop';
+
+@NgModule({
+  imports: [
+    // @angular/materials
+    // MatAutocompleteModule,
+    // MatBadgeModule,
+    // MatBottomSheetModule,
+    MatButtonModule,
+    // MatButtonToggleModule,
+    // MatCardModule,
+    // MatCheckboxModule,
+    // MatChipsModule,
+    // MatTableModule,
+    // MatDatepickerModule,
+    // MatDialogModule,
+    // MatDividerModule,
+    // MatExpansionModule,
+    // MatFormFieldModule,
+    // MatGridListModule,
+    MatIconModule,
+    // MatInputModule,
+    // MatListModule,
+    MatMenuModule,
+    // MatPaginatorModule,
+    // MatProgressBarModule,
+    // MatProgressSpinnerModule,
+    // MatRadioModule,
+    // MatRippleModule,
+    // MatSelectModule,
+    // MatSidenavModule,
+    // MatSlideToggleModule,
+    // MatSliderModule,
+    // MatSnackBarModule,
+    // MatSortModule,
+    // MatStepperModule,
+    // MatTabsModule,
+    MatToolbarModule
+    // MatTooltipModule,
+    // MatTreeModule,
+
+    // @angular/cdk
+  ],
+  exports: [
+    // @angular/materials
+    // MatAutocompleteModule,
+    // MatBadgeModule,
+    // MatBottomSheetModule,
+    MatButtonModule,
+    // MatButtonToggleModule,
+    // MatCardModule,
+    // MatCheckboxModule,
+    // MatChipsModule,
+    // MatTableModule,
+    // MatDatepickerModule,
+    // MatDialogModule,
+    // MatDividerModule,
+    // MatExpansionModule,
+    // MatFormFieldModule,
+    // MatGridListModule,
+    MatIconModule,
+    // MatInputModule,
+    // MatListModule,
+    MatMenuModule,
+    // MatPaginatorModule,
+    // MatProgressBarModule,
+    // MatProgressSpinnerModule,
+    // MatRadioModule,
+    // MatRippleModule,
+    // MatSelectModule,
+    // MatSidenavModule,
+    // MatSlideToggleModule,
+    // MatSliderModule,
+    // MatSnackBarModule,
+    // MatSortModule,
+    // MatStepperModule,
+    // MatTabsModule,
+    MatToolbarModule
+    // MatTooltipModule,
+    // MatTreeModule
+
+    // @angular/cdk
+  ]
+})
+export class MaterialModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-/*
-All the “dumb” components and pipes should be implemented here. 
-These components don’t import and inject services from core or other features in their constructors. 
-They should receive all data though attributes in the template of the component using them. 
-This all sums up to the fact that SharedModule doesn’t have any dependency to the rest of our application.
-*/
+import { MaterialModule } from './material';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule],
   declarations: [],
   providers: [],
-  exports: [CommonModule, FormsModule, ReactiveFormsModule]
+  exports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule]
 })
 export class SharedModule {}
