7b33f8f71c4f63cd01dd0cd9df51289e3528e64a
Lyubomyr Kuzenko <l.kuzenko@levi9.com>
2018-10-23 13:41:54 +0300

    prettier test

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,7 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './material';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule],
+  imports: [
+    CommonModule, 
+    FormsModule, 
+    ReactiveFormsModule, 
+    MaterialModule
+  ],
   declarations: [],
   providers: [],
   exports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule]
