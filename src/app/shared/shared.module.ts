e89630f1dd3c80a53a82615ab44d7c9d52ef3e66
Lyubomyr Kuzenko <l.kuzenko@levi9.com>
2018-10-24 12:16:16 +0300

    add directive, pipes and components to shered module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,26 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './material';
 
+import * as fromDirectives from './directives';
+import * as fromPipes from './pipes';
+import * as fromComponents from './components';
+
 @NgModule({
   imports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule],
-  declarations: [],
+  declarations: [
+    ...fromDirectives.directives,
+    ...fromPipes.pipes,
+    ...fromComponents.components
+  ],
   providers: [],
-  exports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule]
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    ...fromDirectives.directives,
+    ...fromPipes.pipes,
+    ...fromComponents.components
+  ]
 })
 export class SharedModule {}
