71c468e32b31480dd896474c4e8d699beaa1974d
Lyubomyr Kuzenko <l.kuzenko@levi9.com>
2020-10-21 12:17:33 +0300

    update to ng 10

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,19 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './+dashboard/dashboard.component';
-import { LevelOneComponent } from './level-one/level-one.component';
-import { LevelTwoComponent } from './level-one/level-two/level-two.component';
-import { LevelThreeComponent } from './level-one/level-two/level-three/level-three.component';
-import { NgxContextModule } from 'ngx-context';
 import { SharedModule } from '../shared';
 
 @NgModule({
-  imports: [SharedModule, DashboardRoutingModule, NgxContextModule],
-  declarations: [
-    DashboardComponent,
-    LevelOneComponent,
-    LevelTwoComponent,
-    LevelThreeComponent
-  ]
+  imports: [SharedModule, DashboardRoutingModule],
+  declarations: [DashboardComponent]
 })
 export class DashboardModule {}
