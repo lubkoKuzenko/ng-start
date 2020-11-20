090c43221aef23db260638c521161fe665fe294d
Lyubomyr Kuzenko <l.kuzenko@levi9.com>
2020-11-20 12:52:51 +0200

    add validations

--- src/app/forms/components/custom-field-form/counter-input/counter-input.component.ts
+++ src/app/forms/components/custom-field-form/counter-input/counter-input.component.ts
@@ -1,17 +1,34 @@
 import { Component, forwardRef, Input, Provider } from "@angular/core";
-import { NG_VALUE_ACCESSOR, ControlValueAccessor } from "@angular/forms";
+import { NG_VALUE_ACCESSOR, ControlValueAccessor, FormControl, NG_VALIDATORS } from "@angular/forms";
 
-export const COUNTER_VALUE_ACCESSOR: Provider = {
-  provide: NG_VALUE_ACCESSOR,
-  useExisting: forwardRef(() => CounterInputComponent),
-  multi: true,
-};
+export const COUNTER_VALUE_ACCESSOR: Provider = [
+  {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => CounterInputComponent),
+    multi: true,
+  },
+  { provide: NG_VALIDATORS, useExisting: forwardRef(() => CounterInputComponent), multi: true },
+];
+
+export function createCounterRangeValidator(maxValue, minValue) {
+  return (c: FormControl) => {
+    let err = {
+      rangeError: {
+        given: c.value,
+        max: maxValue || 10,
+        min: minValue || 0,
+      },
+    };
+
+    return c.value > +maxValue || c.value < +minValue ? err : null;
+  };
+}
 
 @Component({
   selector: "bb-counter-input",
   templateUrl: "./counter-input.component.html",
   styleUrls: ["./counter-input.component.scss"],
-  providers: [COUNTER_VALUE_ACCESSOR],
+  providers: [...COUNTER_VALUE_ACCESSOR],
 })
 export class CounterInputComponent implements ControlValueAccessor {
   @Input("counterValue") _counterValue = 0;
@@ -19,6 +36,7 @@ export class CounterInputComponent implements ControlValueAccessor {
   @Input() counterRangeMin;
 
   propagateChange: any = () => {};
+  validateFn: any = () => {};
 
   get counterValue() {
     return this._counterValue;
@@ -27,6 +45,12 @@ export class CounterInputComponent implements ControlValueAccessor {
   set counterValue(val) {
     this._counterValue = val;
     this.propagateChange(val);
+  }
+
+  public ngOnChanges(inputs) {
+    if (inputs.counterRangeMax || inputs.counterRangeMin) {
+      this.validateFn = createCounterRangeValidator(this.counterRangeMax, this.counterRangeMin);
+    }
   }
 
   writeValue(value) {
@@ -47,5 +71,9 @@ export class CounterInputComponent implements ControlValueAccessor {
 
   decrease() {
     this.counterValue--;
+  }
+
+  validate(c: FormControl) {
+    return this.validateFn(c);
   }
 }
