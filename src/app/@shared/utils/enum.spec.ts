d3c86fbd764b43a414934c6f0ba9361b9f2dc277
Lyubomyr Kuzenko <lubko.lintu@gmail.com>
2022-02-07 10:21:42 +0200

    ref

--- src/app/@shared/utils/enum.spec.ts
+++ src/app/@shared/utils/enum.spec.ts
@@ -140,17 +140,6 @@ describe("[Utils] enumToEntries function", () => {
         { label: "Sunday", value: 7 },
       ],
     },
-    {
-      value: DayOfWeek,
-      result: [
-        { label: "Monday", value: 1 },
-        { label: "Tuesday", value: 2 },
-        { label: "Wednesday", value: 3 },
-        { label: "Thursday", value: 4 },
-        { label: "Friday", value: 5 },
-        { label: "Saturday", value: 6 },
-      ],
-    },
   ];
 
   TEST_CASES.forEach(({ value, result }) => {
