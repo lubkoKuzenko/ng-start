2ed91c1805dc9bafbba9b94866efda301dbcd62b
Lyubomyr Kuzenko <lubko.lintu@gmail.com>
2022-02-23 16:24:57 +0200

    ref

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { HomeComponent } from "./home.component";
+
+@NgModule({
+  declarations: [HomeComponent],
+  exports: [HomeComponent],
+})
+export class HomeModule {}
