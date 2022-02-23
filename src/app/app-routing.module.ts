a0a27ef474f6886f8913f06126926f6213c94732
Lyubomyr Kuzenko <lubko.lintu@gmail.com>
2022-02-23 16:17:50 +0200

    ref

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core";
 import { NoPreloading, RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
   {
     path: "",
-    redirectTo: "forms",
-    pathMatch: "full",
+    component: HomeComponent,
   },
   {
     path: "forms",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { CoreModule } from "@core/core.module";
 import { SharedModule } from "@shared/shared.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { HomeModule } from "./home/home.module";
 
 // import mockServer from "./_be-mocks";
 
@@ -25,6 +26,7 @@ const APP_CORE_CONFIG = {
     SharedModule,
 
     // app
+    HomeModule,
     AppRoutingModule,
   ],
   providers: [],
