03f7881bebfef1cc57132a00242baa8d94acbdfb
Lyubomyr Kuzenko <lubko.lintu@gmail.com>
2022-02-23 13:21:55 +0200

    ref

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  mode: "jit",
-  purge: {
-    enabled: true,
-    content: ["./src/**/*.{html,ts}"],
-  },
-  darkMode: false, // or 'media' or 'class'
+  prefix: "",
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
