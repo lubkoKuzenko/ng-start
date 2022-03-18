0c81300ed98886612c875bc7ee4733ecca106fbe
Lyubomyr Kuzenko <lubko.lintu@gmail.com>
2022-03-18 13:10:37 +0200

    ref

--- src/app/patterns/adapter/adapter.component.ts
+++ src/app/patterns/adapter/adapter.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   template: `
-    TODO
+    TODO https://florimond.dev/en/posts/2018/09/consuming-apis-in-angular-the-model-adapter-pattern/
   `,
 })
 export class AdapterComponent {}
