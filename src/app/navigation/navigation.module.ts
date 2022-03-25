b93c9629840d911ff76c8ae7597eac97c1844286
Lyubomyr Kuzenko <lubko.lintu@gmail.com>
2022-03-25 18:15:17 +0200

    ref

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { NavigationComponent } from "./navigation.component";
 import { SharedModule } from "@shared/shared.module";
-import { NavigationComponentsModule } from "./components/components.module";
 import { RouterModule } from "@angular/router";
 
 @NgModule({
-  imports: [CommonModule, SharedModule, RouterModule, NavigationComponentsModule],
+  imports: [CommonModule, SharedModule, RouterModule],
   declarations: [NavigationComponent],
   exports: [NavigationComponent],
 })
