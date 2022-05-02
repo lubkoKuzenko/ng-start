b1d7fc2aaf2237ece755d4e80555d0c440a1ae55
Lyubomyr Kuzenko <lubko.lintu@gmail.com>
2022-05-02 21:54:35 +0300

    add new way of formating code with jest

--- src/app/unit-tests/pipes/string-to-number.pipe.spec.ts
+++ src/app/unit-tests/pipes/string-to-number.pipe.spec.ts
@@ -1,24 +1,41 @@
 import { ToNumberPipe } from "./string-to-number.pipe";
 
-const TEST_CASES = [
-  { value: 12, result: 12 },
-  { value: 12.2, result: 12.2 },
-  { value: "12", result: 12 },
-  { value: "", result: NaN },
-  { value: " ", result: NaN },
-  { value: "12a", result: NaN },
-  { value: "vv12", result: NaN },
-];
+// const TEST_CASES = [
+//   { value: 12, result: 12 },
+//   { value: 12.2, result: 12.2 },
+//   { value: "12", result: 12 },
+//   { value: "", result: NaN },
+//   { value: " ", result: NaN },
+//   { value: "12a", result: NaN },
+//   { value: "vv12", result: NaN },
+// ];
 
+// describe("ToNumberPipe", () => {
+//   const pipe = new ToNumberPipe();
+//   it("should create a pipe instance", () => {
+//     expect(pipe).toBeTruthy();
+//   });
+
+//   TEST_CASES.forEach(({ value, result }) => {
+//     it(`should match the ${value} with to ${result}`, () => {
+//       expect(pipe.transform(value)).toEqual(result);
+//     });
+//   });
+// });
+
+const pipe = new ToNumberPipe();
 describe("ToNumberPipe", () => {
-  const pipe = new ToNumberPipe();
-  it("should create a pipe instance", () => {
+  test.each`
+    value     | result
+    ${12}     | ${12}
+    ${12.2}   | ${12.2}
+    ${"12"}   | ${12}
+    ${""}     | ${NaN}
+    ${" "}    | ${NaN}
+    ${"12a"}  | ${NaN}
+    ${"vv12"} | ${NaN}
+  `("should match the $value with to $result", ({ value, result }) => {
     expect(pipe).toBeTruthy();
-  });
-
-  TEST_CASES.forEach(({ value, result }) => {
-    it(`should match the ${value} with to ${result}`, () => {
-      expect(pipe.transform(value)).toEqual(result);
-    });
+    expect(pipe.transform(value)).toEqual(result);
   });
 });
