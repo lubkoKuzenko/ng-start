de7466a68cf2cfaaf9a78dabd3b7431146d3db52
Lyubomyr Kuzenko <lyubomyrkuzenko@coherentsolutions.com>
2023-03-30 12:41:49 +0300

    resolver.resolveComponentFactory

--- src/app/dynamic-components/containers/devices/devices.component.ts
+++ src/app/dynamic-components/containers/devices/devices.component.ts
@@ -11,6 +11,19 @@ import {
 import { BlueDeviceComponent } from "../../components/blue-device/blue-device.component";
 import { RedDeviceComponent } from "../../components/red-device/red-device.component";
 import { GreenDeviceComponent } from "../../components/green-device/green-device.component";
+import { Type } from "@angular/core";
+
+export enum ItemEnum {
+  Red,
+  Green,
+  Blue,
+}
+
+export const COMPONENT_MAP = new Map<ItemEnum, Type<unknown>>([
+  [ItemEnum.Red, RedDeviceComponent],
+  [ItemEnum.Green, GreenDeviceComponent],
+  [ItemEnum.Blue, BlueDeviceComponent],
+]);
 
 @Component({
   selector: "bb-devices",
@@ -21,20 +34,22 @@ export class DevicesComponent implements OnInit, OnDestroy {
   @ViewChild("dynamicLoadDevicesComponent", { read: ViewContainerRef, static: true }) public entry: ViewContainerRef;
   componentRef: ComponentRef<any>;
 
+  public itemEnum = ItemEnum;
   constructor(private resolver: ComponentFactoryResolver) {}
 
   public ngOnInit() {
-    this.createComponent("green");
+    this.createComponent(ItemEnum.Green);
   }
 
-  public createComponent(type: string) {
+  public createComponent(type: ItemEnum) {
     this.entry.clear();
-    const factory = this.resolver.resolveComponentFactory(this.defineComponent(type));
+    const component = COMPONENT_MAP.get(type) as Type<unknown>;
+    const factory = this.resolver.resolveComponentFactory(component);
     this.componentRef = this.entry.createComponent(factory);
 
-    if (type === "virtual") {
-      this.componentRef.instance.data = Array.from({ length: 1000 }).map((_, i) => `Item #${i}`);
-    }
+    // if (type === "virtual") {
+    //   this.componentRef.instance.data = Array.from({ length: 1000 }).map((_, i) => `Item #${i}`);
+    // }
   }
 
   public defineComponent(type: string) {
